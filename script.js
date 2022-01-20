4b37839c824304688b5219dfd5f8f6a07ac930eb
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-20 13:20:56 +0000

    declared a save btn variable and made function to grab the id of the hour and text to set to local storage through on click

--- script.js
+++ script.js
@@ -2,5 +2,17 @@
 $(document).ready(function() {
     // added current day to jumbotron
     $('#currentDay').text(moment().format('dddd, MMM Do'))
-    
+
+    var saveBtn = $('.saveBtn')
+    // made function to grab the id of the hour and text to set to local storage
+    saveBtn.on('click', function() {
+        var btn = $(this).parent().attr('id')
+        console.log('btn', btn)
+
+        var text = $(this).siblings('.description').val()
+        console.log('text', text)
+
+        localStorage.setItem(btn, text)
+    })
+
 })
