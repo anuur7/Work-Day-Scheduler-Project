b8686655cc933b447064cc299dbc4a05a2266154
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-20 13:18:21 +0000

    made a function for jquery to listen to the documents and added the current day text

---
+++ script.js
@@ -0,0 +1,6 @@
+// made a function for jquery to listen to the documents
+$(document).ready(function() {
+    // added current day to jumbotron
+    $('#currentDay').text(moment().format('dddd, MMM Do'))
+    
+})
