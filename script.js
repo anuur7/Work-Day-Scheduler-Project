f3110372d6c9940edf57843cf370bc6e718121d6
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-22 15:10:41 +0000

    depending on current hr of the day we change the colours by looping over time blocks

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 // made a function for jquery to listen to the documents
-$(document).ready(function() {
-    // added current day to jumbotron
-    $('#currentDay').text(moment().format('dddd, MMM Do'))
+$(document).ready(function () {
+  // added current day to jumbotron
+  $("#currentDay").text(moment().format("dddd, MMM Do"));
 
-    var saveBtn = $('.saveBtn')
-    // made function to grab the id of the hour and text to set to local storage
-    saveBtn.on('click', function() {
-        var btn = $(this).parent().attr('id')
-        console.log('btn', btn)
+  var saveBtn = $(".saveBtn");
+  // made function to grab the id of the hour and text to set to local storage
+  saveBtn.on("click", function () {
+    var btn = $(this).parent().attr("id");
+    console.log("btn", btn);
 
-        var text = $(this).siblings('.description').val()
-        console.log('text', text)
+    var text = $(this).siblings(".description").val();
+    console.log("text", text);
 
-        localStorage.setItem(btn, text)
-    })
+    localStorage.setItem(btn, text);
+  });
 
-    // got all the items by id from local storage and displayed on class description
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'))
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'))
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'))
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'))
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'))
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'))
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'))
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'))
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'))
-})
+  // got all the items by id from local storage and displayed on class description
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
+    $(".description").each(function (calender) {
+    // get current time
+    let currentTime = moment().hours();
+    console.log(currentTime)
+    
+    if (currentTime > calender + 9) {
+      // Past color = grey
+      $(this).addClass("past");
+    }
+    if (currentTime == calender + 9) {
+      // present color = red
+      $(this).addClass("present");
+    }
+    if (currentTime < calender + 9) {
+      // future color = green
+      $(this).addClass("future");
+    }
+  });
+});
